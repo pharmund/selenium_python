9449596d5c96c2901906766cf846dcb7c089680b
pharmund <pharmund@student.21-school.ru>
2023-06-30 12:42:16 +0300

    2

--- lesson.py
+++ lesson.py
@@ -1,13 +1,20 @@
 from selenium import webdriver
+import time
 
-def launchBrowser():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = "C:\\Users\\Redmi\\Desktop\\Selenium2\\yandexdriver.exe"
-    chrome_options.add_argument("disable-infobars");
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com")
-    while True:
-        pass
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = "C:\\Users\\Redmi\\Desktop\\Selenium2\\yandexdriver.exe"
+chrome_options.add_argument("disable-infobars");
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get("https://www.saucedemo.com")
+time.sleep(10)
+user_name = driver.find_element_by_id('user-name')
+user_name.send_keys("standard_user")
+
+time.sleep(10)
+# while True:
+#     pass
+driver.close()
 
 
-launchBrowser()
+
