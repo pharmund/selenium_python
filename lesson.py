a56f2aa4e53b3869358feec9eb38b91106c01355
pharmund <pharmund@student.21-school.ru>
2023-06-30 13:15:19 +0300

    find_element_by

--- lesson.py
+++ lesson.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
 
-def launchBrowser():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = "C:\\Users\\Redmi\\Desktop\\Selenium2\\yandexdriver.exe"
-    chrome_options.add_argument("disable-infobars");
-    driver = webdriver.Chrome()
-    driver.get("https://www.saucedemo.com")
-    while True:
-        pass
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = "C:\\Users\\Redmi\\Desktop\\Selenium2\\yandexdriver.exe"
+chrome_options.add_argument("disable-infobars");
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com")
+user_name = driver.find_element(By.ID, "user-name")
+user_name.send_keys("standard_user")
+time.sleep(5)
+# while True:
+    # pass
 
 
-launchBrowser()
+
